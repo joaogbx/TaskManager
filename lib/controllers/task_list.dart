15fb6463ad7e4444b536a83461b3e9e72aaabbce
Joao <joaogbr1k@gmail.com>
2025-04-24 18:39:45 -0300

    Lista limpada e metodo addTask definido como um Future<void> assincrono

--- lib/controllers/task_list.dart
+++ lib/controllers/task_list.dart
@@ -2,50 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'package:task_manager/models/task.dart';
 
-class TaskList {
-  final List<Task> _tasks = [
-    Task(
-      title: 'teste2',
-      description: 'teste',
-      timer: '${DateTime.now().hour}:${DateTime.now().minute}',
-      locale: 'sala222f',
-      color: Colors.red,
-    ),
-    Task(
-      title: 'teste3',
-      description: 'teste2',
-      timer: '${DateTime.now().hour}:${DateTime.now().minute}',
-      locale: 'sala333f',
-      color: Colors.green,
-    ),
-    Task(
-      title: 'teste3',
-      description: 'teste2',
-      timer: '${DateTime.now().hour}:${DateTime.now().minute}',
-      locale: 'sala333f',
-      color: Colors.green,
-    ),
-    Task(
-      title: 'teste3',
-      description: 'teste2',
-      timer: '${DateTime.now().hour}:${DateTime.now().minute}',
-      locale: 'sala333f',
-      color: Colors.green,
-    ),
-    Task(
-      title: 'teste3',
-      description: 'teste2',
-      timer: '${DateTime.now().hour}:${DateTime.now().minute}',
-      locale: 'sala333f',
-      color: Colors.green,
-    ),
-  ];
+class TaskList extends ChangeNotifier {
+  final List<Task> _tasks = [];
 
   List<Task> get tasks => [..._tasks];
 
-  Task get tasksFavorite => _tasks.firstWhere((e) => e.color == Colors.red);
+  //Task get tasksFavorite => _tasks.firstWhere((e) => e.color == Colors.red);
 
-  addTask(Task task) {
+  Future<void> addTask(Task task) async {
     _tasks.add(task);
+    notifyListeners();
   }
 }
